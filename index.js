4aa083003c1b361f3794839928aa5d345e6826c2
andookie <156530133+andydeng428@users.noreply.github.com>
2024-07-23 22:25:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ AWS.config.update({
   region: 'us-east-1'
 });
 const ses = new AWS.SES();
-
+const s3 = new AWS.S3();
 const yf = require('yahoo-finance');
 const moment = require('moment-timezone');
 const stockList = ['ANET', 'CSCO', 'NVDA', 'BA'];
@@ -11,14 +11,15 @@ const maxDipPercent = 10.0;
 const todayDate = moment().tz('America/New_York').format('YYYY-MM-DD');
 const tomorrowDate = moment().tz('America/New_York').add(1, 'days').format('YYYY-MM-DD');
 
-// Email details
 const emailSender = 'a44deng@uwaterloo.ca';
 const emailRecipients = ['a44deng@uwaterloo.ca'];
+const s3BucketName = 'stock-price-log-bucket428';
 
 exports.handler = async () => {
     const stockChangeList = await retrieveStockDipList();
     if (stockChangeList.length > 0) {
         await sendEmail(stockChangeList);
+        await saveToS3('stock-data', `${todayDate}.json`, stockChangeList);
     }
 };
 
@@ -40,12 +41,12 @@ async function retrieveStockDipList() {
             });
         }
     }
-    // Return list of stock price changes
     return stockInfoList;
 }
 
 // Function to send email to verified email addresses
 async function sendEmail(stockDipList) {
+    const emailContent = `Below are the stocks that changed more than ${maxDipPercent}% from their 52-week high: \n\n${JSON.stringify(stockDipList)} \n\n-from my lambda`;
     const params = {
         Source: emailSender,
         Destination: {
@@ -59,15 +60,28 @@ async function sendEmail(stockDipList) {
             Body: {
                 Text: {
                     Charset: 'UTF-8',
-                    Data: `Below are the stocks that changed more than ${maxDipPercent}% from their 52 week high: \n\n${JSON.stringify(stockDipList)} \n\n-from my lambda`
+                    Data: emailContent
                 }
             }
         }
     };
-    // Catching email sending error
     await ses.sendEmail(params).promise().then(response => {
         console.log(`Email sent: ${response}`);
     }, error => {
         console.error('Error while sending email: ', error);
     });
+
+    // Archive the email content to S3
+    await saveToS3('emails', `${todayDate}.txt`, emailContent);
+}
+
+// Function to save data to S3
+async function saveToS3(folder, fileName, data) {
+    const params = {
+        Bucket: s3BucketName,
+        Key: `${folder}/${fileName}`,
+        Body: JSON.stringify(data),
+        ContentType: 'application/json'
+    };
+    return s3.putObject(params).promise();
 }
